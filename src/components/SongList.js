2102235df59c84dff6e7ecbeee94355ef67d450c
Paola Garay <garay.angela@gmail.com>
2020-09-30 07:03:14 +0200

    Add SongDetail component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
 
 class SongList extends Component {
     renderList() {
@@ -7,7 +9,10 @@ class SongList extends Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">
+                        <button 
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >
                             Select
                         </button>
                     </div>
@@ -30,4 +35,4 @@ const mapStateToProps = state => {
     return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
